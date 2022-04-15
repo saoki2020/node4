164b6fb4765b93e4e1d79f2d4736c1b429f674cd
saoki <54405537+saoki2020@users.noreply.github.com>
2022-04-15 20:50:02 +0900

    Revert "Revert "投稿機能を実装""

--- bbsApp/routes/index.js
+++ bbsApp/routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const authController = require('../controllers/authController');
 const router = express.Router();
-const indexController = require('../controllers/indexController');
 const authRoute = require('../routes/authRoute');
-const userRoute = require('../routes/userRoute');
+const postRoute = require('../routes/postRoute');
+const likeRoute = require('../routes/likeRoute');
 
-router.get('/', indexController.goIndex);
+router.get('/', authController.goLogin);
 
-module.exports = [router, authRoute, userRoute]
+module.exports = [router, authRoute, postRoute, likeRoute]
